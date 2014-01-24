4fc2ff977a9c8e785da0227c777942cb3ec1996a
Joan Serra <serrajoan@me.com>
2014-01-24 16:49:37 +0100

    Initialize repository

---
+++ ticketing/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticketing::Application.config.secret_key_base = '6d0a8d30dc40800cb531f66a8419c2e2979449a59815f52e33a8d1327b0c796e631a1fc5320ae83cf3880b9a753247854bd5a520e840d6b8c61ac7223f62d94a'
---
+++ ticketing/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ticketing::Application.config.session_store :cookie_store, key: '_ticketing_session'
